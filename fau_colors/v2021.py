b2a930b344b3fc2080d372cd8fd53b225473352d
Robert Richer <robert.richer@fau.de>
2021-10-19 09:39:55 +0200

    renamed department to faculty in code, updated example images

--- fau_colors/v2021.py
+++ fau_colors/v2021.py
@@ -6,16 +6,18 @@ from typing_extensions import Literal
 
 from fau_colors._utils import custom_blend_colormap, get_register_func, get_unregister_func
 
+__all__ = ["colors", "colors_all", "colors_dark", "cmaps", "register_cmaps", "unregister_cmaps"]
+
 NAMED_COLORS_T = Literal["fau", "tech", "phil", "med", "nat", "wiso"]
 NAMED_COLORS = ["fau", "tech", "phil", "med", "nat", "wiso"]
 
-_DepartmentColors = namedtuple("DepartmentColors", NAMED_COLORS)
-_DepartmentColorsAll = namedtuple("DepartmentColorsAll", [f"{a}{b}" for a, b in product(NAMED_COLORS, ("", "_dark"))])
-_CmapsAll = namedtuple("Cmaps", ["departments", "departments_dark", "departments_all", *_DepartmentColorsAll._fields])
+_FacultyColors = namedtuple("FacultyColors", NAMED_COLORS)
+_FacultyColorsAll = namedtuple("FacultyColorsAll", [f"{a}{b}" for a, b in product(NAMED_COLORS, ("", "_dark"))])
+_CmapsAll = namedtuple("Cmaps", ["faculties", "faculties_dark", "faculties_all", *_FacultyColorsAll._fields])
 
 _LIGHTNESS_LEVELS = [0.125, 0.25, 0.375, 0.625, 1]
 
-colors = _DepartmentColors(
+colors = _FacultyColors(
     fau="#002F6C",
     tech="#779FB5",
     phil="#FFB81C",
@@ -23,17 +25,17 @@ colors = _DepartmentColors(
     nat="#43B02A",
     wiso="#C8102E",
 )
-colors_dark = _DepartmentColors(
+colors_dark = _FacultyColors(
     fau="#041E42", tech="#41748D", phil="#E87722", med="#0061A0", nat="#228848", wiso="#971B2F"
 )
-colors_all: _DepartmentColorsAll = _DepartmentColorsAll(
+colors_all: _FacultyColorsAll = _FacultyColorsAll(
     **colors._asdict(), **{f"{k}_dark": v for k, v in colors_dark._asdict().items()}
 )
 
 cmaps = _CmapsAll(
-    departments=sns.color_palette(list(colors), as_cmap=True),
-    departments_dark=sns.color_palette(list(colors_dark), as_cmap=True),
-    departments_all=sns.color_palette(list(colors_all), as_cmap=True),
+    faculties=sns.color_palette(list(colors), as_cmap=True),
+    faculties_dark=sns.color_palette(list(colors_dark), as_cmap=True),
+    faculties_all=sns.color_palette(list(colors_all), as_cmap=True),
     **{
         k: sns.color_palette(custom_blend_colormap(["#FFFFFF", v], list(reversed(_LIGHTNESS_LEVELS))), as_cmap=True)
         for k, v in colors_all._asdict().items()
