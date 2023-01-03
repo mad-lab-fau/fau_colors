ef8e47a4aa248f7962f6e55663cd0fb3aa0ac152
Robert Richer <robert.richer@fau.de>
2023-01-03 13:26:27 +0100

    after formatting

--- fau_colors/__init__.py
+++ fau_colors/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "1.4.1"
 
 from fau_colors._utils import export_as_gpl
-from fau_colors.v2021 import cmaps, colors, colors_all, colors_dark, colors_light, register_cmaps, unregister_cmaps
 from fau_colors.fonts import register_fausans_font
+from fau_colors.v2021 import cmaps, colors, colors_all, colors_dark, colors_light, register_cmaps, unregister_cmaps
